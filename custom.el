287705c9097f6e554c8c11c14b783881b5c8035d
Gianpietro Barel <gianpietro@MacAir.fritz.box>
2025-06-29 16:44:38 +0200

    Track also custom.el with git

---
+++ custom.el
@@ -0,0 +1,31 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(ef-spring))
+ '(custom-safe-themes
+   '("b3ba955a30f22fe444831d7bc89f6466b23db8ce87530076d1f1c30505a4c23b"
+     default))
+ '(denote-directory "/Users/gianpietro/Documents/Denote")
+ '(ews-bibtex-directory "/Users/gianpietro/Documents/Denote")
+ '(ews-hunspell-dictionaries "en_US,it_IT")
+ '(package-selected-packages
+   '(balanced-windows biblio citar-denote consult-notes denote-explore
+		      denote-journal denote-org denote-sequence
+		      ef-themes elfeed-org emms fountain-mode helpful
+		      lorem-ipsum magit marginalia markdown-mode
+		      mixed-pitch modus-themes nov olivetti openwith
+		      orderless org-appear org-fragtog org-modern
+		      org-web-tools ox-epub spacious-padding titlecase
+		      vertico vundo writegood-mode))
+ '(spacious-padding-mode nil nil nil "Customized with use-package spacious-padding"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 160 :family "Aporetic Serif Mono"))))
+ '(fixed-pitch ((t (:family "Aporetic Serif Mono"))))
+ '(variable-pitch ((t (:family "Aporetic Sans")))))
